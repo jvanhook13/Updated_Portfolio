189d2a688dcfe5d25823d47da294e175002847fe
jvanhook13 <jaredvanhook1313@gmail.com>
2020-03-26 00:16:47 -0700

    Added buttons to projects, added mouse over states for fun, changed background, updated text styling.

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+var myimgobj = document.images["midButton"]
+
+console.log(myimgobj)
+
+function changeImage1()
+{
+document.images["midButton1"].src= "http://placekitten.com/300/300";
+return true;
+}
+function changeImageBack1()
+{
+ document.images["midButton1"].src = "http://placekitten.com/300/300";
+ return true;
+}
+
+function changeImage2()
+{
+document.images["midButton2"].src= "http://placekitten.com/300/300";
+return true;
+}
+function changeImageBack2()
+{
+ document.images["midButton2"].src = "http://placekitten.com/300/300";
+ return true;
+}
+
+function changeImage3()
+{
+document.images["midButton3"].src= "http://placekitten.com/300/300";
+return true;
+}
+function changeImageBack3()
+{
+ document.images["midButton3"].src = "http://placekitten.com/300/300";
+ return true;
+}
